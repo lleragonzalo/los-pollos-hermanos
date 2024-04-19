343b88bda82e9f2fee47de3b3ea597b14722467f
albertotorresfever <162992114+albertotorresfever@users.noreply.github.com>
2024-04-19 09:26:10 +0200

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import './main.scss';
+import menu from './menu.json';
+import './assets/fonts/Marcheile-Bold-Condensed.woff';
+import './assets/fonts/Marcheile-Bold-Condensed.woff2';
+/* DO NOT EDIT ABOVE THIS LINE. You can start editing here. */
+
+/**
+ * === CODE HINT ===
+ * You can write your own js code here, also you can import other files.
+ * If you want to split your code into multiple files, then import them here this way:
+ * import ./path/to/your/file.js
+ *
+ */
+
